e9c527f79660f7c59ef53666d45ac56f3837b4f2
Alex Whelan <awhelan@ucdavis.edu>
2018-06-02 05:26:48 -0700

    Loaded All Data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os,sys
 import psycopg2
 import csv
+from collections import defaultdict
 
 
 # Get DataPath
@@ -14,8 +15,12 @@ except:
 try:
     conn = psycopg2.connect("host=localhost dbname=FakeUData user=postgres")
     print("Successfully Connecte to Database!\n", conn)
-    cur = conn.cursor()
+except:
+    print("ERROR: Failed to Connect To Database!\n")
 
+
+try:
+    cur = conn.cursor()
     # Create Course Relation
     cur.execute("""
     CREATE TABLE course(
@@ -33,25 +38,40 @@ try:
     # Create Relation Meeting
     cur.execute("""
     CREATE TABLE meeting(
-        CID integer PRIMARY KEY,
+        CID integer,
+        TERM integer,
         INSTRUCTOR varchar(64),
         TYPE varchar(64),
         DAYS varchar(5),
         TIME varchar(20),
         BUILD varchar(16),
-        ROOM int
+        ROOM varchar(8),
+        PRIMARY KEY(CID, TERM, INSTRUCTOR, TYPE, DAYS, TIME, BUILD, ROOM)
+    )
+    """)
+
+    # Create Student Meeting
+    cur.execute("""
+    CREATE TABLE students(
+        SID integer PRIMARY KEY,
+        SURNAME varchar(64),
+        PREFNAME varchar(64),
+        EMAIL varchar(128)
     )
     """)
 
     conn.commit()
 except:
-    print("ERROR: Failed to Connect To Database!\n")
+    print("FAILED TO CREATE TABLE\n")
+
 
 
 start_year = 1989
 end_year = 2012
 quarters = ['Q1','Q2','Q3','Q4']
 
+
+student_dict = defaultdict(lambda:0)
 
 for year in range(start_year, end_year+1):
     for quarter in quarters:
@@ -70,7 +90,9 @@ for year in range(start_year, end_year+1):
         reader = csv.reader(f)
 
         # Initialize Batch Send Object
-        batch_course = ""
+        batch_course = []
+        batch_meeting = []
+        batch_student = []
 
         # Skip Blank Header
         next(reader)
@@ -85,6 +107,7 @@ for year in range(start_year, end_year+1):
             # Skip Blank Header
             skip = next(reader)
 
+            meeting_dict = defaultdict(lambda:0)
             meeting_list = [[course_data[0], course_data[1]]]
             meeting_header = next(reader)
 
@@ -94,7 +117,12 @@ for year in range(start_year, end_year+1):
                 if meeting_data == ['']:
                     break
                 else:
-                    meeting_list.append(meeting_data)
+                    if tuple(meeting_data) not in meeting_dict:
+                        meeting_dict[tuple(meeting_data)]
+                        meeting_list.append(meeting_data)
+                    else:
+                        continue
+
 
             student_list = [[course_data[0], course_data[1]]]
             student_header = next(reader)
@@ -113,27 +141,42 @@ for year in range(start_year, end_year+1):
 
             if len(student_list) > 1:
 
-                # print("\n\n***********************************************\n\n")
-                # print("\n\nCourse INFO: \n\n ",course_data)
-                #
-                # print("\n\n***********************************************\n\n")
-                # print("\n\nMEETING INFO: \n\n ",meeting_list)
-                #
-                # print("\n\n***********************************************\n\n")
-                # print("\n\nSTUDENTS: \n\n ",student_list, len(student_list))
                 capacity = len(student_list) - 1
-                print(course_data, type(course_data), capacity, type(capacity))
-                course_data.append(capacity)
-                print(course_data)
+                course_data.append(str(capacity))
+                batch_course.append(tuple(course_data))
 
-                batch_course += str(tuple(course_data)) + ","
+                for m in meeting_list[1:]:
+                    meeting_tuple = [course_data[0], course_data[1]]
+                    meeting_tuple.extend(m)
+                    batch_meeting.append(tuple(meeting_tuple))
+
+                # Add Static Student Info
+                for s in student_list[1:]:
+                    if tuple(s[1:4]) not in student_dict:
+                        # Add student to static dictionary
+                        student_dict[tuple(s[1:4])]
+                        student_tuple = s[1:4]
+                        student_tuple.append(s[10])
+                        batch_student.append(tuple(student_tuple))
+                    else:
+                        continue
 
 
+
+        # ADD Batch to DB
         conn = psycopg2.connect("host=localhost dbname=FakeUData user=postgres")
         cur = conn.cursor()
 
-        batch_course = batch_course[:-1]
-        cur.execute(
-        "INSERT INTO course VALUES " + batch_course
-        )
+        # Add Course Info
+        SQL = "INSERT INTO course VALUES (%s, %s, %s, %s, %s, %s, %s);"
+        cur.executemany(SQL, batch_course)
+
+        # Add Meeting Info
+        SQL = "INSERT INTO meeting VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"
+        cur.executemany(SQL, batch_meeting)
+
+        # Add Static Student Info
+        SQL = "INSERT INTO students VALUES (%s, %s, %s, %s);"
+        cur.executemany(SQL, batch_student)
+
         conn.commit()
