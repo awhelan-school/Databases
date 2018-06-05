1c00597497b7b20e4d22680e225c57d44531b74e
Alex Whelan <awhelan@ucdavis.edu>
2018-06-05 01:57:11 -0700

    5A started

--- queryfakeu.py
+++ queryfakeu.py
@@ -166,78 +166,234 @@ cur = conn.cursor()
 
 
 
-# Problem 3D
-course_dict = defaultdict(lambda:0)
+# # Problem 3D
+# course_dict = defaultdict(lambda:0)
+#
+# # GET all course as unique
+# cur.execute("""
+# SELECT DISTINCT crse, subj
+# FROM course
+# """)
+#
+# course_list = cur.fetchall()
+# for f in course_list:
+#     cur.execute("""
+#     SELECT count(*)
+#     FROM (SELECT cid, term
+# 		FROM course
+# 		WHERE subj = %s and crse = %s) R NATURAL JOIN enrollment
+#         WHERE  grade = 'A+' or grade = 'A' or grade = 'A-' or
+#         	   grade = 'B+' or grade = 'B' or grade = 'B-' or
+#         	   grade = 'C+' or grade = 'C' or grade = 'C-' or
+#         	   grade = 'D+' or grade = 'D' or grade = 'D-' or
+#         	   grade = 'F'  or grade = 'P' or grade = 'NP' or
+#         	   grade = 'S'  or grade = 'U' or grade = 'NS'
+#     """, [f[1], f[0]])
+#     total_students = cur.fetchone()[0]
+#
+#     cur.execute("""
+#     SELECT count(*)
+#     FROM (SELECT cid, term
+# 		FROM course
+# 		WHERE subj = %s and crse = %s) R NATURAL JOIN enrollment
+#         WHERE  grade = 'A+' or grade = 'A' or grade = 'A-' or
+#         	   grade = 'B+' or grade = 'B' or grade = 'B-' or
+#         	   grade = 'C+' or grade = 'C' or grade = 'C-' or
+#         	   grade = 'D+' or grade = 'D' or grade = 'D-' or
+#                grade = 'P'  or grade = 'S'
+#     """, [f[1], f[0]])
+#
+#     passing_students = cur.fetchone()[0]
+#     if(total_students > 0):
+#
+#         rate = passing_students/total_students
+#         course_dict[(f[1], f[0])] = rate
+#
+# best = max(course_dict.values())
+# worst = min(course_dict.values())
+#
+# test = 0
+# print("\nHIGHEST PASS RATE COURSES")
+# print("((SUBJ, CRSE), PASS RATE)\n")
+# for i in course_dict.items():
+#     if i[1] == best:
+#         print(i)
+#
+# print("\nLOWEST PASS RATE COURSES")
+# print("((SUBJ, CRSE), PASS RATE)\n")
+# for i in course_dict.items():
+#     if i[1] == worst:
+#         print(i)
+#
+#
+# cur.execute("""
+#     SELECT DISTINCT m1.subj, m1.crse, m2.subj, m2.crse
+#     FROM
+#     (
+#     	SELECT cid, term, instructor, type, days, time, build, room, subj, crse
+#     	FROM meeting NATURAL JOIN course
+#     ) m1
+#     JOIN
+#     (
+#     	SELECT cid, term, instructor, type, days, time, build, room, subj, crse
+#     	FROM meeting NATURAL JOIN course
+#     ) m2
+#     ON m1.cid <> m2.cid and m1.term = m2.term and m1.term::text NOT LIKE '____06' and
+#        m1.instructor = m2.instructor and m1.instructor <> '' and
+#        m1.days = m2.days and m1.days <> '' and
+#        m1.time = m2.time and m1.time <> '' and
+#        m1.build = m2.build and m1.build <> '' and
+#        m1.room = m2.room and m1.room <> '' and
+#        m1.subj <> m2.subj and m1.crse <> m2.crse and
+#        m1.cid < m2.cid and m1.crse < m2.crse
+#     ORDER BY m1.subj, m1.crse ASC
+# """)
+#
+# cross_list = cur.fetchall()
+#
+# print("\nPROBLEM 3E\n")
+# for cx in cross_list:
+#     print(cx)
+#
+#
+#
+# # PROBLEM 3F
+#
+# cur.execute("""
+#     WITH R AS (SELECT major, avg(value) as gpa
+#     	FROM
+#     	(
+#     	SELECT course.cid, course.term, subj, sid, grade, major
+#     	FROM course JOIN enrollment ON course.cid = enrollment.cid and course.term = enrollment.term
+#     	WHERE SUBJ = 'ABC'
+#     	) R
+#     	NATURAL JOIN gpa
+#     	GROUP BY major
+#     	ORDER BY gpa ASC
+#     )
+#     SELECT major, gpa as perfomance
+#     FROM R
+#     WHERE gpa = (SELECT max(gpa) FROM R)
+#     UNION
+#     SELECT major, gpa as perfomance
+#     FROM R
+#     WHERE gpa = (SELECT min(gpa) FROM R)
+#     ORDER BY perfomance ASC
+# """)
+#
+# abc_major_performance = cur.fetchall()
+#
+# cur.execute("""
+#     WITH R AS (SELECT major, avg(value) as gpa
+#     	FROM
+#     	(
+#     	SELECT course.cid, course.term, subj, sid, grade, major
+#     	FROM course JOIN enrollment ON course.cid = enrollment.cid and course.term = enrollment.term
+#     	WHERE SUBJ = 'DEF'
+#     	) R
+#     	NATURAL JOIN gpa
+#     	GROUP BY major
+#     	ORDER BY gpa ASC
+#     )
+#     SELECT major, gpa as perfomance
+#     FROM R
+#     WHERE gpa = (SELECT max(gpa) FROM R)
+#     UNION
+#     SELECT major, gpa as perfomance
+#     FROM R
+#     WHERE gpa = (SELECT min(gpa) FROM R)
+#     ORDER BY perfomance ASC
+# """)
+#
+# def_major_performance = cur.fetchall()
+#
+# print("\nPROBLEM: 3F")
+# print("MAJOR PERFORMANCE ABC\n")
+#
+# for i in abc_major_performance:
+#     print(i)
+#
+# print("\nMAJOR PERFORMANCE DEF\n")
+#
+# for i in def_major_performance:
+#     print(i)
 
-# GET all course as unique
+
+# 3G TRANSFER INTO ABC
 cur.execute("""
-SELECT DISTINCT crse, subj
-FROM course
+    WITH transfers AS
+    (
+    SELECT e1.major as previous_major, count(*) as number_switched
+    FROM enrollment as e1, enrollment as e2
+    WHERE e1.sid = e2.sid and e1.major <> e2.major and e1.term < e2.term and e2.major LIKE 'ABC%' and e1.major NOT LIKE 'ABC%'
+    GROUP BY e1.major
+    ORDER BY number_switched DESC
+    )
+
+    SELECT previous_major, number_switched, number_switched / (SUM(number_switched) OVER ()) AS percent_switched
+    FROM transfers
+    ORDER BY number_switched DESC
 """)
 
-course_list = cur.fetchall()
-for f in course_list:
-    cur.execute("""
-    SELECT count(*)
-    FROM (SELECT cid, term
-		FROM course
-		WHERE subj = %s and crse = %s) R NATURAL JOIN enrollment
-        WHERE  grade = 'A+' or grade = 'A' or grade = 'A-' or
-        	   grade = 'B+' or grade = 'B' or grade = 'B-' or
-        	   grade = 'C+' or grade = 'C' or grade = 'C-' or
-        	   grade = 'D+' or grade = 'D' or grade = 'D-' or
-        	   grade = 'F'  or grade = 'P' or grade = 'NP' or
-        	   grade = 'S'  or grade = 'U' or grade = 'NS'
-    """, [f[1], f[0]])
-    total_students = cur.fetchone()[0]
+top_transfers = cur.fetchall()[0:5]
+transfer_dict = defaultdict(lambda:0)
 
-    cur.execute("""
-    SELECT count(*)
-    FROM (SELECT cid, term
-		FROM course
-		WHERE subj = %s and crse = %s) R NATURAL JOIN enrollment
-        WHERE  grade = 'A+' or grade = 'A' or grade = 'A-' or
-        	   grade = 'B+' or grade = 'B' or grade = 'B-' or
-        	   grade = 'C+' or grade = 'C' or grade = 'C-' or
-        	   grade = 'D+' or grade = 'D' or grade = 'D-' or
-               grade = 'P'  or grade = 'S'
-    """, [f[1], f[0]])
-
-    passing_students = cur.fetchone()[0]
-    if(total_students > 0):
-
-        rate = passing_students/total_students
-        course_dict[(f[1], f[0])] = rate
-
-best = max(course_dict.values())
-worst = min(course_dict.values())
-
-test = 0
-for i in course_dict.items():
-    print(i)
-    if i[1] == 1:
-        test += 1
-
-print(len(course_dict), test)
+print("PROBLEM 3G: Transfers into ABC")
+# Top Transfers
+for t in top_transfers:
+    transfer_dict[t[0]] = float(t[2])
 
 
+plt.bar(range(len(transfer_dict)), list(transfer_dict.values()), align='center')
+plt.xticks(range(len(transfer_dict)), list(transfer_dict.keys()))
+plt.show()
 
 
-# SELECT DISTINCT s1,cs1,s2,cs2
-# FROM(
-# 	SELECT term, c1,c2,s1,cs1,subj as s2, crse as cs2
-# 	FROM (
-# 		SELECT c1,c2,subj as s1 ,crse as cs1
-# 		FROM (
-# 			SELECT m1.cid as c1, m2.cid as c2, m1.term as t1, m2.term as t2
-# 			FROM meeting as m1, meeting as m2
-# 			WHERE m1.cid <> m2.cid and m1.term = m2.term and m1.term::text NOT LIKE '____06' and
-# 				   m1.instructor = m2.instructor and m1.instructor <> '' and
-# 				   m1.days = m2.days and m1.days <> '' and
-# 				   m1.time = m2.time and m1.time <> '' and
-# 				   m1.build = m2.build and m1.build <> '' and
-# 				   m1.room = m2.room and m1.room <> '' and
-# 				   m1.cid < m2.cid ) R JOIN course on c1 = course.cid ) S Join course on c2 = course.cid
-# ) X
-# WHERE s1 <> s2 and cs1 <> cs2 and cs1 < cs2
-# ORDER BY s1, cs1 ASC
+# 3H TRANSFER FROM ABC
+cur.execute("""
+    WITH transfers AS
+    (
+    SELECT e2.major as new_major, count(*) as number_switched
+    FROM enrollment as e1, enrollment as e2
+    WHERE e1.sid = e2.sid and e1.major <> e2.major and e1.term < e2.term and e2.major NOT LIKE 'ABC%' and e1.major LIKE 'ABC%'
+    GROUP BY e2.major
+    ORDER BY number_switched DESC
+    )
+
+    SELECT new_major, number_switched, number_switched / (SUM(number_switched) OVER ()) AS percent_switched
+    FROM transfers
+    ORDER BY number_switched DESC
+""")
+
+top_transfers = cur.fetchall()[0:5]
+transfer_dict = defaultdict(lambda:0)
+
+print("PROBLEM 3H: Transfers into ABC")
+# Top Transfers
+for t in top_transfers:
+    transfer_dict[t[0]] = float(t[2])
+
+
+plt.bar(range(len(transfer_dict)), list(transfer_dict.values()), align='center')
+plt.xticks(range(len(transfer_dict)), list(transfer_dict.keys()))
+plt.show()
+
+
+# PROBLEM 5A
+
+# WITH student_courses AS
+# (
+# SELECT course.cid, course.term, sid, subj, crse
+# FROM enrollment
+# JOIN course on course.cid = enrollment.cid and course.term = enrollment.term
+# )
+#
+# SELECT R.subj, R.crse, R.currently_taking, student_courses.subj, student_courses.crse, count(*) as pre_requisite, (count(*)::NUMERIC / R.currently_taking::NUMERIC) as percent_pre
+# FROM
+# (
+# 	SELECT cid, term, sid, subj, crse, count(*) over () as currently_taking
+# 	FROM student_courses
+# 	WHERE subj = 'ABC' and crse = 203
+# ) R JOIN student_courses ON R.sid = student_courses.sid and R.term > student_courses.term
+# GROUP BY R.subj, R.crse, R.currently_taking, student_courses.subj, student_courses.crse
+# ORDER BY percent_pre DESC
